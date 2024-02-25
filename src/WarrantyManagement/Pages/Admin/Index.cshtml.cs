a3d2c70a9c736768e2b95407fa47627f0c802669
Tope <castillochristopherjames@gmail.com>
2024-02-25 14:23:12 +0800

    setup mobile view of admin layout

--- src/WarrantyManagement/Pages/Admin/Index.cshtml.cs
+++ src/WarrantyManagement/Pages/Admin/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MyApp.Namespace
 {
-    //[Authorize(Roles = UserRoles.Admin)]
+    [Authorize(Roles = UserRoles.Admin)]
     public class IndexModel : PageModel
     {
         private readonly warrantyContext _warrantyCtx;
